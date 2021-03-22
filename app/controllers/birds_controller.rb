9774ee7f13e2d0c16cc1787ca79f1c9ec426e179
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-22 09:41:57 -0500

    commit tree 0d1ecd6ebbc3b70f199794327b6d224c0da82a1e

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,18 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    # render json: {id: bird.id ,name: bird.name, species: bird.species}
+    if bird
+
+    render json: bird.slice(:id, :name, :species)
+    else
+      render json: {message: "Bird not found"}
+    end
+
   end
 end
